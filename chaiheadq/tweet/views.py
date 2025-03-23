5f88f39b4f346248168e11fe339a66858e7ef5a8
Muhammad <muhammadabdullahgohar572@gmail>
2025-03-24 02:32:49 +0500

    first commit

--- chaiheadq/tweet/views.py
+++ chaiheadq/tweet/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from .models import Tweet
+from .forms import TweetFrom
+from django.shortcuts import get_object_or_404
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def Tweet_list(request):
+    Tweets=Tweet.objects.all().order_by('-created_at')
+    return render(request,'tweet_list.html',{'tweets':Tweets})
